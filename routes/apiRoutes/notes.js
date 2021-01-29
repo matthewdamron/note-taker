ab6414b5ede6905bedd9ca1f04004ff110067c73
Matthew Damron <mattdamron@msn.com>
2021-01-28 17:40:27 -0700

    finished routes

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 // const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
 const { notes } = require('../../data/notes');
 
-// router.get('/notes', (req, res) => {
-//   let results = notes;
+router.get('/notes', (req, res) => {
+  let results = notes;
 //   if (req.query) {
 //     results = filterByQuery(req.query, results);
 //   }
-//   res.json(results);
-// });
+  res.json(results);
+});
 
 // router.get('/animals/:id', (req, res) => {
 //   const result = findById(req.params.id, animals);
