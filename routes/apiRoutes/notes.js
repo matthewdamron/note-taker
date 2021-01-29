7e1bf7591341938317901fdf8d3fb8d7bc050516
Matthew Damron <mattdamron@msn.com>
2021-01-29 07:27:17 -0700

    fixed get and post error

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { createNewNote } = require('../../lib/notes');
 // const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
 const { notes } = require('../../data/notes');
 
@@ -8,6 +9,18 @@ router.get('/notes', (req, res) => {
 //     results = filterByQuery(req.query, results);
 //   }
   res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    // if (!validateAnimal(req.body)) {
+    //   res.status(400).send('The animal is not properly formatted.');
+    // } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    // }
 });
 
 // router.get('/animals/:id', (req, res) => {
